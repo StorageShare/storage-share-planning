3a358433845711ca1e6fcafba3e7b8bc6677120a
Vincent de Jong <vincent@vinvu.nl>
2025-10-08 10:27:26 +0200

    Add fallback view for empty planning scenarios and update controller logic

---
+++ resources/views/my-planning/show-empty.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            Geen planning beschikbaar.
+        </h2>
+        <br>
+        <p>Op dit moment staat er voor jou geen planning klaar.</p>
+    </x-slot>
+</x-app-layout>
