6e9934cf44c1db7cf39f089399f6ac0faf402ead
Vincent de Jong <vincent@vinvu.nl>
2026-03-30 13:12:05 +0200

    Remove obsolete photo workflow emails and command
    - Deleted views, mailables, and `ProcessPhotoWorkflowCommand` related to the photo workflow system, as they are now handled by `storage-share-api`.

--- app/Http/Controllers/TaskPhotoProcessController.php
+++ app/Http/Controllers/TaskPhotoProcessController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Enums\TaskPriority;
 use App\Enums\TaskStatus;
-use App\Mail\InternalCheckRequestMail;
-use App\Mail\RoomPhotoDistributedMail;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
